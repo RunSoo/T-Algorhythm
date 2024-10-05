477f2c336f952afd2038cc90dc08436b42f07f3c
RunSoo <109501116+RunSoo@users.noreply.github.com>
2024-10-05 12:04:18 +0900

    [level 3] Title: 네트워크, Time: 0.94 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -1,28 +1,44 @@
-function solution(n, computers) {
-    let answer = 0;
-    const visited = new Array(n).fill(false);
-
-    function bfs(start) {
-        const queue = [start];
-        visited[start] = true;
-
-        while (queue.length) {
-            const node = queue.shift();
-            for (let i = 0; i < n; i++) {
-                if (computers[node][i] === 1 && !visited[i]) {
-                    queue.push(i);
-                    visited[i] = true;
-                }
+class UnionFind {
+    constructor(n){
+        this.parent = Array.from({length: n}, (_, i)=>i);
+        this.rank = new Array(n).fill(1);
+    }
+    
+    find(x){
+        if (this.parent[x]!==x){
+            this.parent[x]=this.find(this.parent[x]);
+        }
+        return this.parent[x];
+    }
+    
+    union(x, y){
+        let rootX = this.find(x);
+        let rootY = this.find(y);
+        if (rootX!==rootY){
+            if (this.rank[rootX]>this.rank[rootY]){
+                this.parent[rootY] = rootX;
+            } else if (this.rank[rootX]<this.rank[rootY]){
+                this.parent[rootX] = rootY;
+            } else {
+                this.parent[rootY] = rootX;
+                this.rank[rootX]++;
             }
         }
     }
+}
 
-    for (let i = 0; i < n; i++) {
-        if (!visited[i]) {
-            answer++;
-            bfs(i);
+function solution(n, computers) {
+    const uf = new UnionFind(n);
+    for (let r=0; r<computers.length; r++){
+        for (let c=0; c<computers[0].length; c++){
+            if (computers[r][c]===1){
+                uf.union(r, c);
+            }
         }
     }
-
-    return answer;
+    const set = new Set();
+    for (let i=0; i<uf.parent.length; i++){
+        set.add(uf.find(uf.parent[i]));
+    }
+    return set.size;
 }
